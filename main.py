1d9fe7bedf7d0f7048f891cbcc0bf8a87dde8edb
iznoel <leon90.zed@gmail.com>
2022-04-20 15:41:01 +0300

    add abstractions for scraper

--- main.py
+++ main.py
@@ -2,33 +2,41 @@ import requests
 import urllib
 from os import environ
 
-KEY = environ['PROJ_TMDB_KEY']
+TMDB_KEY = environ['PROJ_TMDB_KEY']
 
-URL_IMG = 'https://api.themoviedb.org/3/movie/{ID}/images?api_key={KEY}'
-URL_ID = 'https://api.themoviedb.org/3/search/movie?api_key={KEY}&query={QUERY}'
-URL_CNF = 'http://api.themoviedb.org/3/configuration?api_key={KEY}'
+class TMDBScraper:
 
-"""
-https://api.themoviedb.org/3/search/movie?api_key={KEY}&language=en-US&query=die%20hard&page=1&include_adult=false 
-https://api.themoviedb.org/3/search/movie?api_key={KEY}&query={QUERY}
-base_url = conf['images']['base_url']
-"""
+    URL_CNF = 'http://api.themoviedb.org/3/configuration?api_key={KEY}'
+    search = {
+        'by_imdb_id': 'https://api.themoviedb.org/3/movie/{ID}/images?api_key={KEY}',
+        'by_name': 'https://api.themoviedb.org/3/search/movie?api_key={KEY}&query={QUERY}',
+    }
 
-def get_config():
-    query = URL_CNF.format(KEY=KEY)
-    response = requests.get(query)
-    return response.json()
+    def __init__(self, key=TMDB_KEY):
+        self.key = key
+        self.conf = self.get_config()
 
-def search_ids(name):
-    query = URL_ID.format(KEY=KEY,QUERY=urllib.parse.quote(name))
-    response = requests.get(query)
-    return [x['id'] for x in response.json()['results']]
+    def get_config(self):
+        query = self.URL_CNF.format(KEY=self.key)
+        response = requests.get(query)
+        if response.status_code != 200:
+            raise Exception
+        return response.json()
 
-def get_img(config, id):
-    query = URL_IMG.format(KEY=KEY, ID=id)
-    response = requests.get(query)
-    return [ f"{config['images']['base_url']}original{x['file_path']}" for x in response.json()['posters']]
+    def search_ids(self, name):
+        query = self.search['by_name'].format(KEY=self.key, QUERY=urllib.parse.quote(name))
+        response = requests.get(query)
+        return [x['id'] for x in response.json()['results']]
+
+    def get_img(self, id):
+        query = self.search['by_imdb_id'].format(KEY=self.key, ID=id)
+        response = requests.get(query)
+        return [f"{self.conf['images']['base_url']}original{x['file_path']}" for x in response.json()['posters']]
 
 
-conf = get_config()
-ids = search_ids('batman')
+if __name__ == "__main__":
+    scraper = TMDBScraper()
+    ids = scraper.search_ids('batman')
+    urls = scraper.get_img(ids[0])
+    for url in urls:
+        print(url)
