a6ce2fbb8517ad4b0306d04308ea2c4d054097be
iznoel <leon90.zed@gmail.com>
2022-04-20 14:53:23 +0300

    scraper example

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import requests
-CONFIG_PATTERN = 'http://api.themoviedb.org/3/configuration?api_key={key}'
-KEY = '192ee0157eb9c506ee39de6c95e00873'
+import urllib
+from os import environ
 
-url = CONFIG_PATTERN.format(key=KEY)
-r = requests.get('https://api.themoviedb.org/3/configuration?api_key=192ee0157eb9c506ee39de6c95e00873')
-config = r.json()
-print(config)
-base_url = config['images']['base_url']
-sizes = config['images']['poster_sizes']
+KEY = environ['PROJ_TMDB_KEY']
+
+URL_IMG = 'https://api.themoviedb.org/3/movie/{ID}/images?api_key={KEY}'
+URL_ID = 'https://api.themoviedb.org/3/search/movie?api_key={KEY}&query={QUERY}'
+URL_CNF = 'http://api.themoviedb.org/3/configuration?api_key={KEY}'
+
 """
-    'sizes' should be sorted in ascending order, so
-        max_size = sizes[-1]
-    should get the largest size as well.        
+https://api.themoviedb.org/3/search/movie?api_key={KEY}&language=en-US&query=die%20hard&page=1&include_adult=false 
+https://api.themoviedb.org/3/search/movie?api_key={KEY}&query={QUERY}
+base_url = conf['images']['base_url']
 """
-def size_str_to_int(x):
-    return float("inf") if x == 'original' else int(x[1:])
-max_size = max(sizes, key=size_str_to_int)
-IMG_PATTERN = 'http://api.themoviedb.org/3/movie/{imdbid}/images?api_key={key}'
-r = requests.get(IMG_PATTERN.format(key=KEY,imdbid='tt0095016'))
-api_response = r.json()
-print(api_response)
-posters = api_response['posters']
-poster_urls = []
-for poster in posters:
-    rel_path = poster['file_path']
-    url = "{0}{1}{2}".format(base_url, max_size, rel_path)
-    poster_urls.append(url)
-for nr, url in enumerate(poster_urls):
-    r = requests.get(url)
-    filetype = r.headers['content-type'].split('/')[-1]
-    filename = 'poster_{0}.{1}'.format(nr+1,filetype)
-    with open(filename,'wb') as w:
-        w.write(r.content)
+
+def get_config():
+    query = URL_CNF.format(KEY=KEY)
+    response = requests.get(query)
+    return response.json()
+
+def search_ids(name):
+    query = URL_ID.format(KEY=KEY,QUERY=urllib.parse.quote(name))
+    response = requests.get(query)
+    return [x['id'] for x in response.json()['results']]
+
+def get_img(config, id):
+    query = URL_IMG.format(KEY=KEY, ID=id)
+    response = requests.get(query)
+    return [ f"{config['images']['base_url']}original{x['file_path']}" for x in response.json()['posters']]
+
+
+conf = get_config()
+ids = search_ids('batman')
