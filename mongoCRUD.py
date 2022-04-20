9138ed9a9c640d42cae92d9e600d4300ca399508
iznoel <leon90.zed@gmail.com>
2022-04-20 09:57:09 +0300

    separate environment vars

--- mongoCRUD.py
+++ mongoCRUD.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from gridfs import GridFS
+from os import environ
 # import requests
 
-PROJ_HOST = 'localhost'
-PROJ_PORT = 27017
-PROJ_DB   = "posters_database"
+HOST = environ['PROJ_HOST'] or 'localhost'
+PORT = environ['PROJ_PORT'] or '27017'
+DB   = environ['PROJ_DB']   or 'posters_database'
 
 class posterStorage:
 
@@ -31,7 +32,7 @@ class posterStorage:
         return self.files.delete(self.find(name)._id)
 
 # if __name__ == "__main__":
-#     db = posterStorage(db=PROJ_DB, host=PROJ_HOST, port=PROJ_PORT)
+#     db = posterStorage(db=DB, host=HOST, port=PORT)
 #     file = requests.get("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwallpapershero.com%2Fwp-content%2Fuploads%2Fsites%2F13%2F2014%2F11%2FCat-Sad-Annoyed.jpg%3Ffit%3D2560%252C1600%26ssl%3D1&f=1&nofb=1")
 #     db.save(name='test', data=file.content)
 #     db.delete('test')
