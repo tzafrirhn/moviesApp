99712bfd4f234585899302a1ac6f43a1da476e36
chen <tzafrirhn@gmail.com>
2022-04-20 09:57:41 +0300

    updated version working with mongodb saving and downloading files from MongoDB

--- flaskApp.py
+++ flaskApp.py
@@ -1,32 +1,40 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,request,redirect
+import requests
+from mongoCRUD import getPoster
+
+app = Flask(__name__, template_folder="template")
+
 
 @app.route('/')
 def searchPoster():
     return '''<html>
     <body>
-    <form action="search">
+    <form action="search" method="POST">
     <label>Movie Name:</label>
-    <input type="search" name="psearch">
+    <input type="search" name="image">
     <input type="submit">
     </form>
     </body>
     </html>
     '''
 
-@app.route('/search')
-def showPoster():
-    #search and upload to mongo function and show poster
-    return '''<html>
+
+@app.route('/search/<image>', methods=['GET', 'POST'])
+def showPoster(image):
+    #image = request.args.get('image')
+    # search and upload to mongo function and show poster
+    filename = getPoster(image)
+    return f'''<html>
     <body>
     <form action="search">
     <label>Movie Name:</label>
-    <input type="search" name="psearch">
+    <input type="search" name="image">
     <input type="submit">
     </form>
-    <img src="X">
+    <img src="{filename}">
     </body>
-    </html
+    </html>
     '''
 
-app.run(host='localhost')
+
+app.run(host='localhost',port=5000,debug=True)
--- mongoCRUD.py
+++ mongoCRUD.py
@@ -1,26 +1,52 @@
-from pymongo import MongoClient
 import gridfs
+import requests
+import os
+from pymongo import MongoClient
 
 connection_url = "mongodb://localhost:27017/"
-db_name = "posters_database"
 
-# def createDB():
-db = MongoClient(connection_url).gridfs_example
-# Create an object of GridFs for the above database.
-#database = connection[db_name]
+
+def mongoConnect():
+    try:
+        conn = MongoClient(connection_url)
+        print("MongoDB connected", conn)
+        return conn.PosterFiles
+    except Exception as e:
+        print("Error in Mongo Connection:", e)
+
 
 def newPoster(file):
+    db = mongoConnect()
     # Create an object of GridFs for the above database.
     fs = gridfs.GridFS(db)
     # Open the image in read-only format.
-    with open(file, 'rb') as f:
-        contents = f.read()
+    if file.startswith('http://') or file.startswith('https://'):
+        imContent = requests.get(file)
+    else:
+        with open(file, 'rb') as f:
+            imContent = f.read()
 
     # Now store/put the image via GridFs object.
-    fs.put(contents, filename="file")
+    name = file.split('/')
+    print(name[-1])
+    fs.put(imContent.content, filename=name[-1])
 
 
-def readPoster():
-    pass
+def getPoster(name):
+    db = mongoConnect()
+    fs = gridfs.GridFS(db)
+    try:
+        print(name)
+        data = fs.find_one({'filename': name})
+        image = data.read()
+        temp = open("images/poster.jpg", 'wb')
+        temp.write(image)
+        path=os.path.join(os.path.abspath("."),"images","poster.jpg")
+        temp.close()
+    except Exception as e:
+        print(e)
+    return path
 
-newPoster("https://dictionary.cambridge.org/fr/images/thumb/poster_noun_002_28550.jpg")
+
+getPoster("pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")
+# newPoster("https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg")
