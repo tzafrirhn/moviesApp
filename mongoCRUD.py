dfbdd74663db86907b0844c1a650fa82d08f10d4
iznoel <leon90.zed@gmail.com>
2022-04-20 09:08:30 +0300

    basic abstraction over gridfs

--- mongoCRUD.py
+++ mongoCRUD.py
@@ -1,26 +1,37 @@
 from pymongo import MongoClient
-import gridfs
+from gridfs import GridFS
+# import requests
 
-connection_url = "mongodb://localhost:27017/"
-db_name = "posters_database"
+PROJ_HOST = 'localhost'
+PROJ_PORT = 27017
+PROJ_DB   = "posters_database"
 
-# def createDB():
-db = MongoClient(connection_url).gridfs_example
-# Create an object of GridFs for the above database.
-#database = connection[db_name]
+class posterStorage:
 
-def newPoster(file):
-    # Create an object of GridFs for the above database.
-    fs = gridfs.GridFS(db)
-    # Open the image in read-only format.
-    with open(file, 'rb') as f:
-        contents = f.read()
+    def __init__(self, db, host, port):
+        self.opts = {'host': host, 'port': port}
+        self.database = db
 
-    # Now store/put the image via GridFs object.
-    fs.put(contents, filename="file")
+    @property
+    def files(self):
+        connection = MongoClient(**self.opts)
+        return GridFS(connection[self.database])
 
+    def save(self, name, data):
+        return self.files.put(data, filename=name)
 
-def readPoster():
-    pass
+    def find(self, name):
+        return self.files.find_one({'filename': name})
 
-newPoster("https://dictionary.cambridge.org/fr/images/thumb/poster_noun_002_28550.jpg")
+    def overwrite(self, name, data):
+        self.delete(name)
+        self.save(name, data)
+
+    def delete(self, name):
+        return self.files.delete(self.find(name)._id)
+
+# if __name__ == "__main__":
+#     db = posterStorage(db=PROJ_DB, host=PROJ_HOST, port=PROJ_PORT)
+#     file = requests.get("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwallpapershero.com%2Fwp-content%2Fuploads%2Fsites%2F13%2F2014%2F11%2FCat-Sad-Annoyed.jpg%3Ffit%3D2560%252C1600%26ssl%3D1&f=1&nofb=1")
+#     db.save(name='test', data=file.content)
+#     db.delete('test')
