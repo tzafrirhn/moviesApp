3b6b2464ba623c9306717231601060c152b008c9
iznoel <leon90.zed@gmail.com>
2022-04-20 09:49:41 +0300

    handle args in /search

--- flaskApp.py
+++ flaskApp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,18 +16,11 @@ def searchPoster():
 
 @app.route('/search')
 def showPoster():
-    #search and upload to mongo function and show poster
-    return '''<html>
-    <body>
-    <form action="search">
-    <label>Movie Name:</label>
-    <input type="search" name="psearch">
-    <input type="submit">
-    </form>
-    <img src="X">
-    </body>
-    </html
-    '''
+    args = request.args
+    if 'psearch' not in args.keys():
+        return redirect(url_for('searchPoster'))
+    return f"""Searched for: {args['psearch']}"""
+
 
 if __name__ == "__main__":
     app.run()
